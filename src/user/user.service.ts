d5e95f395c6eeccb521b4174215cfd4f5850abae
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-05-10 10:55:38 -0600

    feat(): connect with database

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,14 +2,19 @@ import { Injectable, Param } from '@nestjs/common';
 import { Request } from 'express';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  get() {
-    return {
-      name: 'Rod',
-      email: 'j@r.com',
-    };
+  constructor(
+    @InjectRepository(User)
+    private readonly _userRepository: Repository<User>,
+  ) {}
+
+  get(): Promise<User[]> {
+    return this._userRepository.find();
   }
 
   create(createUserDto: CreateUserDto) {
